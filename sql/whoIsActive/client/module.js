085ccccf9a6aa31853e55836176ca1fbb1bbf0de
Dave Baskin <dfbaskin@stanza.com>
2013-12-27 21:03:32 -0500

    Adding details view of WhoIsActive.

--- sql/whoIsActive/client/module.js
+++ sql/whoIsActive/client/module.js
@@ -34,4 +34,26 @@ angular
                 });
             }
         ]
+    )
+    .directive(
+        'sgBindText',
+        [
+            function() {
+                return {
+                    restrict: 'A',
+                    link: function(scope, element, attrs) {
+                        scope.$watch(attrs.sgBindText, function(v) {
+                            element.text(v);
+                            var r = element[0];
+                            if (r.offsetWidth < r.scrollWidth) {
+                                element.attr('title', v);
+                            }
+                            else {
+                                element.removeAttr('title');
+                            }
+                        });
+                    }
+                }
+            }
+        ]
     );
